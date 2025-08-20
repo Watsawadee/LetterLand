1b14f21532341930b68e01bab3b2296ba919200a
Ntckx <Nontakorn.chatk@gmail.com>
2025-08-20 13:35:34 +0700

    refactor: remove unused saveGameFromGemini route from createGameRoutes

--- server/src/routes/createGameRoutes.ts
+++ server/src/routes/createGameRoutes.ts
@@ -1,6 +1,5 @@
-const express = require("express");
 import { createGameFromGemini } from "../controllers/createGameController";
-import { saveGameFromGemini } from "../controllers/saveGameFromGemini";
+import express from "express"
 import multer from "multer";
 
 const router = express.Router();
@@ -8,6 +7,5 @@ const upload = multer();
 
 
 router.post("/gemini", upload.single("file"), createGameFromGemini);
-router.post("/gemini/save", saveGameFromGemini);
 
 export default router;
