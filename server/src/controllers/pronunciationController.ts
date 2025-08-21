3201979f6ed86230c547b7b3e11b0df2a20f81ed
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2025-08-21 20:43:27 +0700

    [UPDATE] GG drive upload + path Backend
    - Upload audio to GG drive
    - Remove /api in Backend
    - Remove node module in root

--- server/src/controllers/pronunciationController.ts
+++ server/src/controllers/pronunciationController.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 import { generatePronunciation } from "../services/textToSpeechService";
+import { getFileFromDrive } from "../services/ggDriveService";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const AUDIO_FOLDERID = process.env.AUDIO_FOLDERID!;
 export const pronunciation = async (req: Request, res: Response) => {
   const { word } = req.body;
 
@@ -19,5 +24,26 @@ export const pronunciation = async (req: Request, res: Response) => {
   } catch (error) {
     console.error("Pronunciation error:", error);
     res.status(500).json({ message: "Failed to generate pronunciation" });
+  }
+};
+
+export const getAudio = async (req: Request, res: Response) => {
+  const { fileName } = req.params;
+  if (!fileName) return res.status(400).json({ message: "fileName is required" });
+
+  try {
+    const file = await getFileFromDrive(fileName, AUDIO_FOLDERID);
+
+    if (!file) {
+      return res.status(404).json({ message: "File not found" });
+    }
+
+    return res.status(200).json({
+      message: "File found",
+      url: file.webViewLink || file.webContentLink,
+    });
+  } catch (error) {
+    console.error("Error fetching file:", error);
+    return res.status(500).json({ message: "Failed to fetch file" });
   }
 };
