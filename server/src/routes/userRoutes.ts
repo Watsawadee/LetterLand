6c501ec3fb94962cdf50f335295eead15f3e4a29
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2025-08-25 15:22:56 +0700

    [UPDATE] useHint Backend & Backend path
    - useHint Backend
    - Shorten Backend Path

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -3,10 +3,13 @@ import {
   createUserController,
   getUserByIdController,
   loginUserController,
+  getAllUserController,
+  useHintController,
 } from "../controllers/userController";
 const router = express.Router();
 
-router.get("/user/:userId", getUserByIdController);
+router.get("/", getAllUserController);
+router.get("/:userId", getUserByIdController);
 router.post("/auth/register", async (req: Request, res: Response) => {
   try {
     const result = await createUserController(req.body);
@@ -24,5 +27,6 @@ router.post("/auth/login", async (req: Request, res: Response) => {
     res.status(400).json({ error: error.message });
   }
 });
+router.post("/:userId/usehint", useHintController);
 
 export default router;
